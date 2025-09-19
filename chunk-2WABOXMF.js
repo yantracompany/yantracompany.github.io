a71244d71ad92ca2244dc80affdadba9b79ca432
Mohit Saroha <mohitsaroha03@gmail.com>
2025-09-20 01:23:35 +0530

    Deploy: yantra-world static build (2025-09-20 01:23:35)

--- chunk-2WABOXMF.js
+++
@@ -1,1 +0,0 @@
-import{b as a}from"./chunk-77GZTJLZ.js";import"./chunk-S36FDYE4.js";import"./chunk-CPYPWHC4.js";import"./chunk-HXMYN2EI.js";export{a as BlogListComponent};
---
+++ chunk-JHGPOCPC.js
@@ -0,0 +1,1 @@
+import{b as a}from"./chunk-NYUZYCCO.js";import"./chunk-IHMFJCDH.js";import"./chunk-2PB5FYNP.js";export{a as BlogListComponent};
